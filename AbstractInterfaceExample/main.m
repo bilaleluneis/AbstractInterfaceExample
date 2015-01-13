88da68c5ecb9eb21b6790b4dcf7db5488089b1f5
bilaleluneis <bilaleluneis@gmail.com>
2015-01-13 11:04:08 -0500

    Initial Commit

---
+++ AbstractInterfaceExample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AbstractInterfaceExample
+//
+//  Created by Bilal El Uneis on 1/13/15.
+//  Copyright (c) 2015 Bilal El Uneis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
